ce0f8af8c0575c821501b17b4974b756ddf0e045
taichi <taichi.01gr14@gmail.com>
2022-10-25 21:03:43 +0900

    L10

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log('useEffect');
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
